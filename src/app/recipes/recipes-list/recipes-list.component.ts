a299a6bece7a605de02f2ee861370f65f2f49a44
Robin Burri <rburri@c1r16s5.42lausanne.ch>
2023-11-22 13:30:31 +0100

    adding content to list recipe

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
-  template: `<p>recipes-list works!</p>`,
+  templateUrl: './recipes-list.component.html',
   styleUrl: './recipes-list.component.css',
 })
-export class RecipesListComponent { }
+export class RecipesListComponent {
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is a simple test", "https://img.freepik.com/free-photo/pizza-pizza-filled-with-tomatoes-salami-olives_140725-1200.jpg?w=2000&t=st=1700655637~exp=1700656237~hmac=87ba60ae9bc2cbd0e94e144399486709d5ec375d59b5de68e42b0aaf1a269243")
+  ];
+
+  constructor() {};
+ }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,7 +2,7 @@ import {  Component } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list',
-  template: `<p>shopping-list works!</p>`,
+  templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent { }
